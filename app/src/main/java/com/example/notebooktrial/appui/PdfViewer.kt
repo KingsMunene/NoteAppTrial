b3961c26ea2d3fbabf27e0a58751c5bcfab6036b
KingsMunene <munenekings2@gmail.com>
2023-07-14 13:51:22 +0300

    pdf viewer layout and xml defining

---
+++ app/src/main/java/com/example/notebooktrial/appui/PdfViewer.kt
@@ -0,0 +1,20 @@
+package com.example.notebooktrial.appui
+
+import android.view.View
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.notebooktrial.R
+import com.github.barteksc.pdfviewer.PDFView
+
+@Composable
+fun PdfDocViewer(fileName: String) {
+    AndroidView(
+        factory = {
+            View.inflate(it, R.layout.pdf_viewer, null)
+        },
+        update = {
+            val pdfViewer = it.findViewById<PDFView>(R.id.pdfViewer)
+            pdfViewer.fromAsset(fileName).enableSwipe(true).load()
+        }
+    )
+}
