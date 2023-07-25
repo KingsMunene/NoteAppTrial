2339d89adc59703716095cf700b6062d65cce300
KingsMunene <munenekings2@gmail.com>
2023-07-25 14:02:01 +0300

    Took back the pdfViewer composable to the viewModel for smooth rendering

--- app/src/main/java/com/example/notebooktrial/appui/PdfViewModel.kt
+++ app/src/main/java/com/example/notebooktrial/appui/PdfViewModel.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.notebooktrial.R
 import com.github.barteksc.pdfviewer.PDFView
 import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
@@ -19,22 +20,47 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
+// We expect every document to start at first page (0)
+const val DEFAULTPAGENO = 0
+
 class PdfViewModel: ViewModel() {
 
     //Data class to hold the document page number
-    data class DocPage(val pageNumber: Int = 0)
+    data class DocPage(val pageNumber: Int = DEFAULTPAGENO)
 
     // State for the current page
     private var _uiState = MutableStateFlow(DocPage())
 
+    @Composable
+    fun PdfViewer(
+        fileName: String,
+    ) {
 
-    //Backing state for the uiState
-    val uiSate = _uiState.asStateFlow()
+        AndroidView(
+            factory = {
+                View.inflate(it, R.layout.pdf_viewer, null)
+
+            },
+            update = {
+                val pdfViewer = it.findViewById<PDFView>(R.id.pdfViewer)
+
+                pdfViewer.fromAsset(fileName)
+                    .defaultPage(_uiState.value.pageNumber)
+                    .enableSwipe(true)
+                    .scrollHandle(DefaultScrollHandle(it.context))
+                    .onPageChange { page, _ ->
+                        if (page >= 1){
+                           _uiState.update { it.copy(pageNumber = page) }
+                        }
+                        Log.d("Here", "OnPageChange called")
+
+                    }
+                    .spacing(0)
+                    .load()
+            },
+            modifier = Modifier.fillMaxSize()
+        )
 
 
-    // Function to update the current page of the document
-    fun updateCurrentPage(page: Int){
-        _uiState.update { it.copy(pageNumber = page)}
     }
-
 }
